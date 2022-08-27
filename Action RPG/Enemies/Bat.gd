f71f57bee41e38b812a5772983164b781509777b
tejmen <tejmen09@gmail.com>
2022-08-27 17:45:51 +0100

    Added soft bat collisions

--- Action RPG/Enemies/Bat.gd
+++ Action RPG/Enemies/Bat.gd
@@ -21,6 +21,7 @@ onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $HurtBox
+onready var softCollisions = $SoftCollision
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -40,7 +41,9 @@ func _physics_process(delta):
 			else:
 				state = IDLE
 			sprite.flip_h = velocity.x < 0
-			
+	
+	if softCollisions.is_colliding():
+		velocity += softCollisions.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
 
 func seek_player():
