01115faff1a69e81b0d33911f08dace1d36731f5
tejmen <tejmen09@gmail.com>
2023-08-01 08:24:23 +0100

    changed old man's wander state code,otyer minor fixes

--- Action RPG/Objects/Old_Man.gd
+++ Action RPG/Objects/Old_Man.gd
@@ -1,9 +1,28 @@
 extends KinematicBody2D
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 75
+export var FRICTION = 200
+
+var velocity = Vector2.ZERO
+
+onready var sprite = $OldManSprite
 onready var PDZ = $PlayerDetectionZone
 onready var Textbox = $Textbox
+onready var wanderController = $WanderController
+onready var softCollisions = $SoftCollision
 
-func _process(delta):
+func _physics_process(delta):
 	if PDZ.can_see_player() == true:
 		if Input.is_action_just_pressed("action"):
 			Textbox.queue_text("Hello my dear child, where have you come from?")
+	if wanderController.get_time_left() == 0:
+		wanderController.start_wander_timer(rand_range(1,3))
+		
+	var direction = global_position.direction_to(wanderController.target_position)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+	
+	if softCollisions.is_colliding():
+		velocity += softCollisions.get_push_vector() * delta * 400
+	velocity = move_and_slide(velocity)
